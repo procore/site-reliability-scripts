ff5f0396e3f7b4aeebc989b5ff7b89c6794df083
Adam Hess <adamhess1991@gmail.com>
2016-12-28 16:09:52 -0800

    use all command line args when talking to RVM

--- rvm-builder/builder-scripts/install-ruby.sh
+++ rvm-builder/builder-scripts/install-ruby.sh
@@ -6,6 +6,6 @@ RUBY_VER=$1
 echo "Uninstalling ruby ver $RUBY_VER"
 sudo rvm uninstall --gems $RUBY_VER
 echo "Installing ruby ver $RUBY_VER"
-sudo rvm install --movable $RUBY_VER --autolibs=4
+sudo rvm install --movable $@ --autolibs=4
 echo "Preparing..."
 sudo rvm prepare --path $RUBY_VER
